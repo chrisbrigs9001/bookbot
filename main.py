c839888a8b101ac838d0b90b103682f396485e12
chrisbrigs9001 <chrisbrigs9001@gmail.com>
2026-01-04 19:07:31 -0600

    Completed Bookbot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #print("greetings boots")
-from stats import get_word_count
+import sys
+from stats import get_word_count, character_counts, sorted_character_count
 
 
 def get_book_text(filepath):
@@ -8,9 +9,27 @@ def get_book_text(filepath):
     
 
 def main():
-    # print(get_book_text("./books/frankenstein.txt"))
-    word_count = get_word_count(get_book_text("./books/frankenstein.txt"))
-    print(f"Found {word_count} total words")
-    
+    if len(sys.argv)==2:
+        # print(get_book_text("./books/frankenstein.txt"))
+        #frank_file_path = "./books/frankenstein.txt"
+        my_file_path=sys.argv[1]
+        word_count = get_word_count(get_book_text(my_file_path))
+        #print(f"Found {word_count} total words")
+        c_count = character_counts(get_book_text(my_file_path))
+        sorted_counts = sorted_character_count(c_count)
+        #print(sorted_counts)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {my_file_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count} total words")
+        print("--------- Character Count -------")
+        for item in sorted_counts:
+            if item["char"].isalpha():
+                print(f"{item['char']}: {item['num']}")
+        print("============= END ===============")
+        
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     
 main()
--- stats.py
+++ stats.py
@@ -3,5 +3,30 @@ def get_word_count(the_book):
     num_words=len(words)
     return num_words
 
-def character_coounts(the_book):
-    
+def character_counts(the_book):
+    chars = {}
+    for c in the_book:
+        ch = c.lower()
+        if ch in chars:
+            #if the character exists in the dictionary already
+            chars[ch]+=1
+        else:
+            #if the chraracter isn't yet in dictionary, add it to dictionary
+            chars[ch]=1
+    return chars
+
+
+def sort_on(item):
+    return item["num"]
+
+def sorted_character_count(chars):
+    #temp_dictionary = {}
+    temp_list = []
+    #for c in chars:
+    #    print(f"{chars[c]} "+c) */
+    for c in chars:
+        temp_list.append({"char": c, "num": chars[c]})
+    #print(temp_list)
+    #print(type(temp_list))
+    temp_list.sort(reverse=True, key=sort_on)
+    return temp_list
